1819f9b67385b8786f3ff4ae59556afde1174e8e
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2019-11-14 21:43:36 +0300

    Added: google analytics configs

--- src/app.js
+++ src/app.js
@@ -6,16 +6,22 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import RainbowApplication from "react-rainbow-components/components/Application";
 import Admin from "./pages/Admin";
 import { HashRouter, Link } from "react-router-dom";
+import { createBrowserHistory } from 'history';
+import ReactGA from 'react-ga';
 
 // const rootReducer = combineReducers({
 //   form: formReducer
 // });
 
 // const store = createStore(rootReducer);
-
+const history = createBrowserHistory();
+history.listen(location => {
+  ReactGA.set({ page: location.pathname }); // Update the user's current page
+  ReactGA.pageview(location.pathname); // Record a pageview for the given page
+});
 export default function App() {
   return (
-      <Router basename="/new-tab-daily-theme">
+      <Router basename="/new-tab-daily-theme" history={history}>
         <RainbowApplication>
           <Route exact path="/" component={Admin} />
         </RainbowApplication>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./app";
 import registerServiceWorker from "./registerServiceWorker";
+import ReactGA from 'react-ga';
+
+const trackingId = "UA-150458220-2"; 
+ReactGA.initialize(trackingId);
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
