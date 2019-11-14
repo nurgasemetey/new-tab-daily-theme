f9ae7d1a50dff3ff69d87dcbfabb32341efc6e0c
Nurgazy Nazhimidinov <nurgasemetey@gmail.com>
2019-11-14 07:56:11 +0300

    fixed: routing in production with basenames

--- src/app.js
+++ src/app.js
@@ -5,21 +5,20 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import RainbowApplication from "react-rainbow-components/components/Application";
 import Admin from "./pages/Admin";
+import { HashRouter, Link } from "react-router-dom";
 
-const rootReducer = combineReducers({
-  form: formReducer
-});
+// const rootReducer = combineReducers({
+//   form: formReducer
+// });
 
-const store = createStore(rootReducer);
+// const store = createStore(rootReducer);
 
 export default function App() {
   return (
-    <Provider store={store}>
-      <Router>
+      <Router basename="/new-tab-daily-theme">
         <RainbowApplication>
           <Route exact path="/" component={Admin} />
         </RainbowApplication>
       </Router>
-    </Provider>
   );
 }
